7dbbd790b291b0a001b51faa56b705baaa77d5d6
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2026-01-13 23:11:47 +0100

    Improve error handling in `ConfigurationCallExceptionProcessor`: log detailed error messages and refactor error message aggregation

--- src/main/java/cloud/cholewa/gateway/infrastructure/error/processor/ConfigurationCallExceptionProcessor.java
+++ src/main/java/cloud/cholewa/gateway/infrastructure/error/processor/ConfigurationCallExceptionProcessor.java
@@ -4,25 +4,33 @@ import cloud.cholewa.commons.error.model.ErrorMessage;
 import cloud.cholewa.commons.error.model.Errors;
 import cloud.cholewa.commons.error.processor.ExceptionProcessor;
 import cloud.cholewa.gateway.infrastructure.error.ConfigurationCallException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 
 import java.util.Collections;
+import java.util.stream.Collectors;
 
+@Slf4j
 public class ConfigurationCallExceptionProcessor implements ExceptionProcessor {
 
     @Override
     public Errors apply(final Throwable throwable) {
         ConfigurationCallException exception = (ConfigurationCallException) throwable;
 
+        String errorMessages = exception.getErrorMessages().stream()
+            .map(ErrorMessage::getDetails)
+            .collect(Collectors.joining(", "));
+
+        log.error("Error processing configuration call: {}", errorMessages);
+
         return Errors.builder()
             .httpStatus(HttpStatus.BAD_REQUEST)
             .errors(Collections.singleton(
-                    ErrorMessage.builder()
-                        .message("Error processing configuration call")
-                        .details(exception.getErrorMessages().stream().findFirst().orElseThrow().getMessage())
-                        .build()
-                )
-            )
+                ErrorMessage.builder()
+                    .message("Error processing configuration call")
+                    .details(errorMessages)
+                    .build()
+            ))
             .build();
     }
 }
