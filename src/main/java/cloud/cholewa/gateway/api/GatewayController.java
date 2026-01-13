c82100cb8b2832e38d486da4aa254f7b9e0fe3c7
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2026-01-13 23:11:02 +0100

    Add endpoint for consuming AMX messages and corresponding tests

--- src/main/java/cloud/cholewa/gateway/api/GatewayController.java
+++ src/main/java/cloud/cholewa/gateway/api/GatewayController.java
@@ -1,13 +1,14 @@
 package cloud.cholewa.gateway.api;
 
 import cloud.cholewa.gateway.service.GatewayService;
+import cloud.cholewa.home.model.EatonDatagramReply;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
@@ -19,13 +20,15 @@ public class GatewayController {
 
     private final GatewayService gatewayService;
 
-    @GetMapping
-    Mono<ResponseEntity<Void>> incomingGet(
-        @RequestParam(name = "message") final String interfaceType,
-        @RequestBody final String message
-    ) {
-        return gatewayService.parseEatonMessage(interfaceType, message)
+    @PostMapping
+    Mono<ResponseEntity<Void>> consumeAmxMessage(@RequestBody @Valid EatonDatagramReply reply) {
+        return gatewayService.consumeAmxMessage(reply)
             .doOnSubscribe(subscription ->
-                log.info("Incoming AMX message from module: {} and content: {}", interfaceType, message));
+                log.info(
+                    "Consuming AMX message from module: {} and content: [{}]",
+                    reply.getGateway(),
+                    reply.getMessage()
+                ))
+            .then(Mono.fromCallable(() -> ResponseEntity.ok().build()));
     }
 }
--- src/test/java/cloud/cholewa/gateway/api/GatewayControllerTest.java
+++ src/test/java/cloud/cholewa/gateway/api/GatewayControllerTest.java
@@ -1,16 +1,61 @@
 package cloud.cholewa.gateway.api;
 
+import cloud.cholewa.eaton.infrastructure.error.EatonParsingException;
+import cloud.cholewa.eaton.infrastructure.error.ErrorDictionary;
+import cloud.cholewa.gateway.config.ExceptionHandlerConfig;
+import cloud.cholewa.gateway.service.GatewayService;
+import cloud.cholewa.home.model.EatonDatagramReply;
+import cloud.cholewa.home.model.EatonGatewayType;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webflux.test.autoconfigure.WebFluxTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
+import reactor.core.publisher.Mono;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
+@Import(ExceptionHandlerConfig.class)
+@WebFluxTest(GatewayController.class)
 class GatewayControllerTest {
 
+    @Autowired
     private WebTestClient webTestClient;
+
+    @MockitoBean
+    private GatewayService gatewayService;
 
     @Test
     void should_successfully_process_message() {
+        when(gatewayService.consumeAmxMessage(any()))
+            .thenReturn(Mono.empty());
 
-//        webTestClient
+        webTestClient.post()
+            .uri("/amx")
+            .body(BodyInserters.fromValue(EatonDatagramReply.builder()
+                .gateway(EatonGatewayType.BLINDS)
+                .message("5A,C,C1,2C,62,3,0,1,58,0,0,43,5,A5")
+                .build()))
+            .exchange()
+            .expectStatus().isOk();
+    }
+
+    @Test
+    void should_return_error_when_processing_message_fails() {
+        when(gatewayService.consumeAmxMessage(any()))
+            .thenReturn(Mono.error(new EatonParsingException(ErrorDictionary.EXTRACTING_MESSAGE_ERROR)));
+
+        webTestClient.post()
+            .uri("/amx")
+            .body(BodyInserters.fromValue(EatonDatagramReply.builder()
+                .gateway(EatonGatewayType.BLINDS)
+                .message("5A,C,C1,2C,62,3,0,1,58,0,0,43,5,A5")
+                .build()))
+            .exchange()
+            .expectStatus()
+            .isBadRequest();
     }
 }
